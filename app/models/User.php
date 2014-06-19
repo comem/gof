5cc36ffa8e556086cef4a41f57b6bbae8483b4a9
fplomb <florent.plomb@heig-vd.ch>
2014-06-19 17:03:02 +0200

    UserAdd
    retour de user

---
+++ app/models/User.php
@@ -0,0 +1,46 @@
+<?php
+
+
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
+
+class User extends MyEloquent  implements UserInterface, RemindableInterface{
+
+	protected $table = 'users';
+	public $timestamps = true;
+
+	use SoftDeletingTrait, UserTrait, RemindableTrait;
+
+	protected $dates = ['deleted_at'];
+	protected $hidden = array('password');
+
+	public function group()
+	{
+		return $this->belongsTo('Group');
+	}
+
+	public function language()
+	{
+		return $this->belongsTo('Language');
+	}
+        
+         public static function validate($data = array())
+    {
+        return parent::validator($data, array(
+            
+            'email' => 'string|between:1,255|sometimes|required',
+            'first_name' => 'string|between:1,255|sometimes|required',
+            'last_name' => 'string|between:1,255|sometimes|required',
+            'password' => 'string|between:1,255|sometimes|required',
+
+            'last_login' => 'datetime|sometimes',
+            'created_at' => 'date|sometimes|required',
+            'updated_at' => 'date|sometimes|required',
+            'deleted_at' => 'date|sometimes'
+        ));
+    }
+
+}
