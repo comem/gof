a86f17a2134ef345d19edc01872d4785102029e8
Manu Bezençon <Groupe GoF>
2014-06-19 17:25:36 +0200

    LinksController
    Fonction update() OK

--- app/controllers/LinksController.php
+++ app/controllers/LinksController.php
@@ -47,7 +47,6 @@ class LinksController extends \BaseController {
 
         // Les ids venant de l'url sont des "String", alors que celui-ci est un "int"
 		// Par contre la conversion se fait que pour des chaines Ok.
-
         if (ctype_digit($id)) {
             $id = (int)$id;
         }
@@ -58,13 +57,13 @@ class LinksController extends \BaseController {
             return Jsend::fail($validationLink);
         }
 
-        // Vérification de l'existence du message
+        // Vérification de l'existence du lien
         $link = Link::find($id);
         if (!isset($link)) {
             return Jsend::error('link not found');
         }
 
-        // Retourne le message encapsulé en JSEND si tout est OK
+        // Retourne le lien encapsulé en JSEND si tout est OK
         return Jsend::success($link->toArray());
 	}
 
@@ -77,7 +76,56 @@ class LinksController extends \BaseController {
 	 */
 	public function update($id)
 	{
-		//
+		// Vérification des droits d'accès (ACL)
+			//A réaliser
+
+        // Les ids venant de l'url sont des "String", alors que celui-ci est un "int"
+		// Par contre la conversion se fait que pour des chaines Ok.
+        if (ctype_digit($id)) {
+            $id = (int)$id;
+        }
+
+        // Validation des données et retour des liens en JSEND
+        $url = Input::get('url');
+        $name_de = Input::get('name_de');
+        $title_de = Input::get('title_de');
+        $artist_id = Input::get('artist_id');
+
+        if (ctype_digit($artist_id)) {
+            $artist_id = (int)$artist_id;
+        }
+
+        $validationLink = Link::validate(array(
+            'url' => $url,
+            'name_de' => $name_de,
+            'title_de' => $title_de,
+            'artist_id' => $artist_id,
+            'id' => $id
+        ));
+
+        if ($validationLink !== true) {
+            return Jsend::fail($validationLink);
+        }
+
+        //Vérification du contenu ???
+
+        // Vérification de l'existence du lien
+        $link = Link::find($id);
+        if (!isset($link)) {
+            return Jsend::error('link not found');
+        }
+        // Vérification de l'existence de l'artist
+        $artist = Artist::find($artist_id);
+        if (!isset($artist)) {
+            return Jsend::error('artist not found');
+        }
+        // Tout est OK, on mets à jour notre lien
+        $link->url = $url;
+        $link->name_de = $name_de;
+        $link->title_de = $title_de;
+        $link->artist_id = $artist_id;
+        $link->save();
+        return Jsend::success();
 	}
 
 
