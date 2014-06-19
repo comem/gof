f060985ce306f0210cb3a6edda601cc2301647b2
fplomb <florent.plomb@heig-vd.ch>
2014-06-19 15:31:05 +0200

    Update start/global.php
    mise a jour

--- app/start/global.php
+++ app/start/global.php
@@ -27,6 +27,7 @@ ClassLoader::addDirectories(array(
 	app_path().'/commands',
 	app_path().'/controllers',
 	app_path().'/models',
+        app_path().'/libraries',
         app_path().'/validation',
 	app_path().'/database/seeds',
 
