41e9dc0456f894bb54921606405928e776faf105
fplomb <florent.plomb@heig-vd.ch>
2014-06-20 10:51:26 +0200

    Instrument
    Changement crud

--- app/controllers/InstrumentsController.php
+++ app/controllers/InstrumentsController.php
@@ -5,75 +5,73 @@ class InstrumentsController extends \BaseController {
     /**
      * Display a listing of the resource.
      *
-     * @return Response
+     * @return Response retourn tout les instruments de la table
      */
     public function index() {
         // Auth
-       
-        
-       return Jsend::success(Instrument::all()->toArray());
+
+
+        return Jsend::success(Instrument::all()->toArray());
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @return Response
+     * @return Response Message de succes
      */
     public function store() {
         // Auth
-        
-        
-        
+
+
+
         $instrument = Input::get('name_de');
-        
-           if (Instrument::existBuisnessId($instrument) !== true) {
-            return Jsend::fail($instrument);
+
+
+        if (Instrument::existBuisnessId($instrument) == true) {
+
+            return Jsend::error("Cette instrument est déjà enregistré");
         }
-        
+
         $validationInstrum = Instrument::validate(array('instrument' => $instrument));
-      
+
         if ($validationInstrum !== true) {
             return Jsend::fail($validationInstrum);
         }
-      
-           
-     
+
+
+
         $newinstrument = new Instrument();
         $newinstrument->name_de = $instrument;
         $newinstrument->save();
-
-
-        // Et on retourne l'id du message nouvellement créé (encapsulé en JSEND)
         return Jsend::success(array('name_de' => $newinstrument->name_de));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return Response
+     * @param  int  $id id de l'instrument
+     * @return Response l'instrument correspondant à l'id
      */
     public function show($id) {
         // Auth
-       if (ctype_digit($id)) {
-            $id = (int)$id;
+        if (ctype_digit($id)) {
+            $id = (int) $id;
         }
-        
-         if (Instrument::existTechId($id) !== true) {
+
+        if (Instrument::existTechId($id) !== true) {
             return Jsend::fail($id);
         }
-        
-        
+
+
         $validationInst = Instrument::validate(array('id' => $id));
-           
+
         if ($validationInst !== true) {
             return Jsend::fail($validationInst);
         }
-     
-        // Vérification de l'existence du message
+
+        // Vérification de l'existence de l'instrument
         $instrument = Instrument::find($id);
-        // Ou si l'on veut que les informations de l'utilisateur soit avec :
-        // $message = Message::with('user')->find($id);
+        
         if (!isset($instrument)) {
             return Jsend::error('resource not found');
         }
@@ -90,24 +88,24 @@ class InstrumentsController extends \BaseController {
     public function update($id) {
         //Auth
         // Validation des données et retour des messages en JSEND
-        
-         if (ctype_digit($id)) {
-            $id = (int)$id;
+
+        if (ctype_digit($id)) {
+            $id = (int) $id;
         }
-        
+
         $inst = Input::get('name_de');
-        
+
         $validationInst = Instrument::validate(array(
                     'name_de' => $inst,
                     'id' => $id
         ));
-       
-        
+
+
         if ($validationInst !== true) {
             return Jsend::fail($validationMsg);
         }
         // Vérification de l'existence du message
-       
+
         $instrument = Instrument::find($id);
         if (!isset($instrument)) {
             return Jsend::error('resource not found');
@@ -126,31 +124,28 @@ class InstrumentsController extends \BaseController {
      */
     public function destroy($id) {
         //auth
-         if (ctype_digit($id)) {
-            $id = (int)$id;
+        if (ctype_digit($id)) {
+            $id = (int) $id;
         }
-         $validationInst = Instrument::validate(array(
+        $validationInst = Instrument::validate(array(
                     'name_de' => $inst,
                     'id' => $id
         ));
-       
-        
+
+
         if ($validationInst !== true) {
             return Jsend::fail($validationMsg);
         }
-        
+
         $instrument = Instrument::find($id);
         if (!isset($instrument)) {
             return Jsend::error('resource not found');
         }
         // Tout est OK, on mets à jour notre message
-     
+
         $instrument->delete();
-        
+
         return Jsend::success();
-        
     }
-    
- 
 
 }
