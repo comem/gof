18e8c274d0f9d80a222d448cfc87a88f2c80c7d4
fplomb <florent.plomb@heig-vd.ch>
2014-06-20 17:10:15 +0200

    MusicianCrud
    nombreuse chamgenet

--- app/controllers/MusiciansController.php
+++ app/controllers/MusiciansController.php
@@ -21,21 +21,66 @@ class MusiciansController extends \BaseController {
         $first_name = Input::get('first_name');
         $last_name = Input::get('last_name');
         $stagename = Input::get('stagename');
+        $instrumentArtist = Input::get('instrument');
+        
+     
+
 
         $validationMusician = Musician::validate(array('first_name' => $first_name,
                     'last_name' => $last_name,
                     'stagename' => $stagename));
 
-
         if ($validationMusician !== true) {
             return Jsend::fail($validationMusician);
         }
+   
+        foreach ($instruments as $instru) {
+
+            $validationInstru = Instrument::validate(array('id' => (int) $instru['id'], 'name_de' => $instru['name_de']));
+            if ($validationInstru !== true) {
+                return Jsend::fail($validationInstru);
+            }
+
+            if (!Instrument::existTechId((int) $instru['id'])) {
+                return Jsend::error($instru['id'] . ' not found');
+            }
+        }
+        
+        foreach ($artists as $artist) {
+
+            $validationArt = Artist::validate(array('id' => (int) $artist['id']));
+            if ($validationArt !== true) {
+                return Jsend::fail($validationArt);
+            }
+
+            if (!Artist::existTechId((int) $artist['id'])) {
+                return Jsend::error($artist['id'] . ' not found');
+            }
+        }
+
 
         $musician = new Musician();
         $musician->first_name = $first_name;
         $musician->last_name = $last_name;
         $musician->stagename = $stagename;
         $musician->save();
+        
+        dd($instruments);
+
+        foreach ($artists as $artist) {
+            foreach ($instruments as $instru) {
+                if (!ArtistMusician::existTechId($instru->id, $artist->id,$musician->id)) {
+                    $artistMusician = new ArtistMusician();
+                    $artistMusician->artist_id = $artist->id;
+                    $artistMusician->musician_id = $musician->id;
+                    $artistMusician->instrument_id = $instru->id;
+                    $artistMusician->save();
+                } else {
+                    return Jsend::error('$artistMusician already exists');
+                }
+            }
+        }
+
         return Jsend::success(array('first_name' => $first_name,
                     'last_name' => $last_name,
                     'stagename' => $stagename));
--- app/models/ArtistMusician.php
+++ app/models/ArtistMusician.php
@@ -26,5 +26,13 @@ class ArtistMusician extends MyEloquent {
            
         ));
     }
+    public static function existTechId($instru_id,$artist_id,$musician_id) {
+       
+        $e = ArtistMusician::where('instrument_id', '=', $instrument_id)
+            ->where('artist_id', '=', $artist_id)
+            ->where('musician_id', '=', $musician_id)
+            ->first();
+        return $e != null;       // Si null, n’existe pas
+    }
 
 }
