402ecb97aa2d00defdbf54ea7fbc5fdb6c08baf8
Manu Bezençon <Groupe GoF>
2014-06-20 15:43:23 +0200

    NightsPlatformController
    Fonction index() + début de la fonction store()

--- app/controllers/NightsPlatformController.php
+++ app/controllers/NightsPlatformController.php
@@ -6,27 +6,68 @@ class NightsPlatformController extends \BaseController {
 
 	/**
 	 * Display a listing of the resource.
-	 *
 	 * @return Response
 	 */
 	public function index()
 	{
-		// Vérification des droits d'accès (ACL)
-			//A réaliser
-
         // Retourne toutes les publication
-        return  Jsend::success(Platform::all()->toArray());
+        return  Jsend::success(NightPlatform::all()->toArray());
 	}
 
 
 	/**
 	 * Store a newly created resource in storage.
-	 *
 	 * @return Response
 	 */
 	public function store()
 	{
-		//
+
+        $platform_id = Input::get('platform_id');
+        $event_id = Input::get('event_id');
+        $external_id = Input::get('external_id');
+        $external_infos = Input::get('external_infos');
+        $url = Input::get('url');
+
+        //Cast de platform_id et de event_id car l'url les envoit en String
+        if (ctype_digit($platform_id)) {
+            $platform_id = (int)$platform_id;
+        }
+        if (ctype_digit($event_id)) {
+            $event_id = (int)$event_id;
+        }
+
+        // Validation des types
+        $validationNightPlatform = Link::validate(array(
+            'platform_id' => $platform_id,
+            'event_id' => $event_id,
+            'external_id' => $external_id,
+            'external_infos' => $external_infos,
+            'url' => $url,
+        ));
+        if ($validationNightPlatform !== true) {
+            return Jsend::fail($validationNightPlatform);
+        }
+
+        // Validation de l'existance de l'artiste
+        if (Artist::existTechId($artist_id) !== true) {
+            return Jsend::error('artist already exists in the database');
+        }
+
+        // Validation de l'inexistance du lien
+        if (Link::existBusinessId($url) == true) {
+            return Jsend::error('link already exists in the database');
+        }
+
+        // Tout est ok, on sauve le lien avec l'id de l'artiste
+        $link = new Link();
+        $link->url = $url;
+        $link->name_de = $name_de;
+        $link->title_de = $title_de;
+        $link->artist_id = $artist_id;
+        $link->save();
+
+        // Et on retourne l'id du lien nouvellement créé (encapsulé en JSEND)
+        return Jsend::success(array('id' => $link->id));
 	}
 
 
