ef2a08d637a6548f7a38632a6103599829b4ad71
Cedric <cliengme@gmail.com>
2014-06-26 11:57:43 +0200

    odification ImagesController
    modification de la methode store()

--- app/controllers/ImagesController.php
+++ app/controllers/ImagesController.php
@@ -2,49 +2,55 @@
 
 class ImagesController extends \BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 * @return Jsend::success Toutes les images
-	 */
-	public function index()
-	{
-		// Retourne toutes les images
-        return  Jsend::success(Image::all()->toArray());
-	}
+    /**
+     * Display a listing of the resource.
+     * @return Jsend::success Toutes les images
+     */
+    public function index() {
+        // Retourne toutes les images
+        return Jsend::success(Image::all()->toArray());
+    }
 
+    /**
+     * Store a newly created resource in storage.
+     * Attention les paramètres seront passés dans le header.
+     * @var alt_de a récupérer comme contenu en get. Correspond à XXX de l'image.
+     * @var caption_de a récupérer comme contenu en get. Correspond à XXX de l'image.
+     * @var artist_id a récupérer comme contenu en get. Correspond à l'id de l'artiste. 
+     * @var uploadedImage l'image a uploader.
+     * @return Jsend::fail Un message d'erreur si les données entrées ne correspondent pas aux données demandées.
+     * @return Jsend::success Sinon, un message de validation d'enregistrement contenant l'id de l'image créé.
+     */
+    public function store() {
 
-	/**
-	 * Store a newly created resource in storage.
-	 * @var alt_de a récupérer comme contenu en get. Correspond à XXX de l'image.
-	 * @var caption_de a récupérer comme contenu en get. Correspond à XXX de l'image.
-	 * @var source a récupérer comme contenu en get. Correspond à la source de l'image (emplacement).
-	 * @var artist_id a récupérer comme contenu en get. Correspond à l'id de l'artiste.  
-	 * @return Jsend::fail Un message d'erreur si les données entrées ne correspondent pas aux données demandées.
-	 * @return Jsend::success Sinon, un message de validation d'enregistrement contenant l'id de l'image créé.
-	 */
-	public function store()
-	{
-
-        $alt_de = Input::get('alt_de');
-        $caption_de = Input::get('caption_de');
-        $source = Input::get('source');
-        $artist_id = Input::get('artist_id');
-
+        $alt_de = Input::header('alt_de');
+        $caption_de = Input::header('caption_de');
+        $artist_id = Input::header('artist_id');
+        $uploadedImage = Input::file('uploadedImage');
         //Cast de artist_id car l'url l'envoit en String
         if (ctype_digit($artist_id)) {
-            $artist_id = (int)$artist_id;
+            $artist_id = (int) $artist_id;
         }
 
         // Validation des types
         $validationImage = Image::validate(array(
-            'alt_de' => $alt_de,
-            'caption_de' => $caption_de,
-            'source' => $source,
-            'artist_id' => $artist_id,
+                    'alt_de' => $alt_de,
+                    'caption_de' => $caption_de,
+                    'artist_id' => $artist_id,
+                    'uploaded_img' => $uploadedImage,
         ));
         if ($validationImage !== true) {
             return Jsend::fail($validationImage);
         }
+
+
+        $explodedMime = explode('/', $uploadedImage->getMimeType());
+        $uploadFolder = 'public/upload/';
+        $extension = '.' . $explodedMime[1];
+        $imageName = date('Y-m-d-H-i-s'). '-' . $alt_de . $extension;
+        $source = $uploadFolder . $imageName;
+
+        $uploadedImage->move($uploadFolder, date('Y-m-d-H-i-s'). '-' . $alt_de . $extension);
 
         // Sauvegarde de l'image avec l'artiste (Aucune validation car l'id d'artist n'est pas obligatoire.)
         $image = new Image();
@@ -56,28 +62,26 @@ class ImagesController extends \BaseController {
 
         // Et on retourne l'id du lien nouvellement créé (encapsulé en JSEND)
         return Jsend::success(array('id' => $image->id));
-	}
+    }
 
-
-	/**
-	 * Display the specified resource.
-	 * @param  int  $id correspondant à l'id technique du lien à voir.
-	 * @return Jsend::fail Un message d'erreur si les données entrées ne correspondent pas aux données demandées.
-	 * @return Jsend::fail Un message d'erreur si l'id technique est déjà en mémoire.
-	 * @return Jsend::success Sinon, un message de validation d'enregistrement contenant l'image correspondant à l'id technique.
-	 */
-	public function show($id)
-	{
-		// Les ids venant de l'url sont des "String", alors que celui-ci est un "int"
-		// Par contre la conversion se fait que pour des chaines Ok.
+    /**
+     * Display the specified resource.
+     * @param  int  $id correspondant à l'id technique du lien à voir.
+     * @return Jsend::fail Un message d'erreur si les données entrées ne correspondent pas aux données demandées.
+     * @return Jsend::fail Un message d'erreur si l'id technique est déjà en mémoire.
+     * @return Jsend::success Sinon, un message de validation d'enregistrement contenant l'image correspondant à l'id technique.
+     */
+    public function show($id) {
+        // Les ids venant de l'url sont des "String", alors que celui-ci est un "int"
+        // Par contre la conversion se fait que pour des chaines Ok.
         if (ctype_digit($id)) {
-            $id = (int)$id;
+            $id = (int) $id;
         }
 
         // Validation des types
         $validationImage = Image::validate(array('id' => $id));
         if ($validationImage !== true) {
-            return Jsend::fail($validationLink);
+            return Jsend::fail($validationImage);
         }
 
         // Validation de l'existance de lu lien
@@ -90,37 +94,32 @@ class ImagesController extends \BaseController {
 
         // Retourne le lien encapsulé en JSEND si tout est OK
         return Jsend::success($image->toArray());
-	}
+    }
 
+    /**
+     * Update the specified resource in storage.
+     * Priorité 1B
+     * @param  int  $id
+     * @return Rien (fonction non réalisée pour le moment)
+     */
+    public function update($id) {
+        /**
+         * Priorité 1B
+         * Correspond au UPDATE des fonctions CRUD
+         */
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 * Priorité 1B
-	 * @param  int  $id
-	 * @return Rien (fonction non réalisée pour le moment)
-	 */
-	public function update($id)
-	{
-		/** 
-		* Priorité 1B
-		* Correspond au UPDATE des fonctions CRUD
-		*/
-	}
-
-
-	/**
-	 * Remove the specified resource from storage.
-	 * Ne se fait pas dans l'application (seulement soft delet)
-	 * @param  int  $id
-	 * @return Rien (fonction non réalisée pour le moment)
-	 */
-	public function destroy($id)
-	{
-		/** 
-		* Ne se fait pas dans l'application (seulement soft delet)
-		* Correspond au DELETE des fonctions CRUD
-		*/
-	}
-
+    /**
+     * Remove the specified resource from storage.
+     * Ne se fait pas dans l'application (seulement soft delet)
+     * @param  int  $id
+     * @return Rien (fonction non réalisée pour le moment)
+     */
+    public function destroy($id) {
+        /**
+         * Ne se fait pas dans l'application (seulement soft delet)
+         * Correspond au DELETE des fonctions CRUD
+         */
+    }
 
 }
