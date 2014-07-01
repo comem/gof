7fde45d64d981177b4e7aeb3dcaf0c5d3665ba60
Cedric <cliengme@gmail.com>
2014-07-01 15:23:26 +0200

    documentation des models

--- app/models/Genre.php
+++ app/models/Genre.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Database\Eloquent\SoftDeletingTrait;
 
+/**
+ * Genre model
+ * 
+ * Corresponds to the "genres" class of the class diagram.
+ *
+ * @category  Model
+ * @version   1.0
+ * @author    gof
+ */
 class Genre extends MyEloquent {
 
     protected $table = 'genres';
@@ -11,10 +20,19 @@ class Genre extends MyEloquent {
 
     protected $dates = ['deleted_at'];
 
+    /**
+     * Allows to define the relationship between Genre and Artist.
+     * @return Artist the artists of the genre.
+     */
     public function artists() {
         return $this->belongsToMany('Artist');
     }
 
+    /**
+     * Allows to validate attributes for a Genre.
+     * @param array data array with every attributes that has to be validate. 
+     * @return boolean true if the input data are valid, false otherwise.
+     */
     public static function validate($data = array()) {
         return parent::validator($data, array(
                     'id' => 'integer:unsigned|sometimes|required',
@@ -23,11 +41,21 @@ class Genre extends MyEloquent {
         ));
     }
 
+    /**
+     * Allows to verify if a Genre exists in the database with his business id.
+     * @param string the business id corresponding to the name_de of the Genre.
+     * @return boolean true if the Genre exists in the database, false otherwise.
+     */
     public static function existBusinessId($id) {
         $e = Genre::where('name_de', '=', $id)->first();
         return $e != null;
     }
 
+    /**
+     * Allows to verify if a Genre exists in the database with his technical id.
+     * @param int the technical id corresponding to the primary key of the Genre.
+     * @return boolean true if the Genre exists in the database, false otherwise.
+     */
     public static function existTechId($id) {
         $e = Genre::find($id);
         return $e != null;
--- app/models/Link.php
+++ app/models/Link.php
@@ -2,40 +2,36 @@
 
 class Link extends MyEloquent {
 
-	protected $table = 'links';
-	public $timestamps = false;
+    protected $table = 'links';
+    public $timestamps = false;
 
     /**
-     * Cette méthode permet de lier le lien à un artiste
-     * @return L'artiste à qui appartient ce lien.
+     * Allows to define the relationship between Link and Artist.
+     * @return Artist the artist who own the link.
      */
-	public function artist()
-	{
-		return $this->belongsTo('Artist');
-	}
+    public function artist() {
+        return $this->belongsTo('Artist');
+    }
 
     /**
-     * Cette méthode valide les types des attributs selon une liste de critères par attribut.
-     * @param array $data Chaîne de caractère possèdant chaque attribut et leurs critères de validation. 
-     * @return True si les données sont valides ou pas. 
-     *         False si les données ne sont pas valides.
+     * Allows to validate attributes for a Link.
+     * @param array data array with every attributes that has to be validate. 
+     * @return boolean true if the input data are valid, false otherwise.
      */
-	public static function validate($data = array())
-    {
+    public static function validate($data = array()) {
         return parent::validator($data, array(
-            'id' => 'integer:unsigned|sometimes|required',
-            'url' => 'string|between:1,255|sometimes|required|unique:links',
-            'name_de' => 'string|between:1,255|sometimes|required',
-            'title_de' => 'string|between:1,255|sometimes',
-            'artist_id' => 'integer:unsigned|sometimes|required',
+                    'id' => 'integer:unsigned|sometimes|required',
+                    'url' => 'string|between:1,255|sometimes|required|unique:links',
+                    'name_de' => 'string|between:1,255|sometimes|required',
+                    'title_de' => 'string|between:1,255|sometimes',
+                    'artist_id' => 'integer:unsigned|sometimes|required',
         ));
     }
 
     /**
-     * Cette méthode vérifie l'existant du lien selon son identifiant technique.
-     * @param Un identifiant technique correspondant à la clé primaire de lien. 
-     * @return True si le lien existe.
-     *         False si le lien n'existe pas.
+     * Allows to verify if a Link exists in the database with his technical id.
+     * @param int the technical id corresponding to the primary key of the Link.
+     * @return boolean true if the Link exists in the database, false otherwise.
      */
     public static function existTechId($linkId) {
         $e = Link::find($linkId);
@@ -43,13 +39,12 @@ class Link extends MyEloquent {
     }
 
     /**
-     * Cette méthode vérifie l'existant du lien selon son identifiant métier.
-     * @param Un identifiant méditer correspondant à un attribut unique.
-     * @return True si le lien existe.
-     *         False si le lien n'existe pas.
+     * Allows to verify if a Link exists in the database with his business id.
+     * @param string the business id corresponding to the url of the Link.
+     * @return boolean true if the Link exists in the database, false otherwise.
      */
     public static function existBusinessId($linkUrl) {
-        $e = Link::where('url','=',$linkUrl)->first();
+        $e = Link::where('url', '=', $linkUrl)->first();
         return $e != null;
     }
 
